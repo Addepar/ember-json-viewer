94b9f376678352809a0968867c1bf6fdcabae35f
Cory Forsyth <cory.forsyth@addepar.com>
2020-12-07 17:36:14 -0500

    Add `collapseDepth`, expand/collapse icon properties
    Co-authored-by: Wendy Deng <wendy9852@hotmail.com>

--- addon/components/json-viewer.js
+++ addon/components/json-viewer.js
@@ -4,4 +4,9 @@ import layout from "../templates/components/json-viewer";
 export default Component.extend({
   tagName: "",
   layout,
+
+  // Can be overridden
+  expandedIcon: "+",
+  collapsedIcon: "-",
+  collapseDepth: null, // indicates all nodes are expanded
 });
