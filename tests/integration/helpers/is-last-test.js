ac2019054f079bb738235be7228c0410bbcdea5f
Cory Forsyth <cory.forsyth@addepar.com>
2021-01-15 12:31:58 -0500

    tests for is-last helper

--- tests/integration/helpers/is-last-test.js
+++ tests/integration/helpers/is-last-test.js
@@ -1,17 +1,36 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
 
-module('Integration | Helper | is-last', function(hooks) {
+module("Integration | Helper | is-last", function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test("it works with arrays", async function (assert) {
+    this.set("objOrArray", [1, 2, 3]);
+    this.set("keyOrIndex", -1);
 
-    await render(hbs`{{is-last inputValue}}`);
+    await render(hbs`{{if (is-last objOrArray keyOrIndex) "YES" "NO"}}`);
+    assert.equal(this.element.textContent.trim(), "NO");
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    this.set("keyOrIndex", 0);
+    assert.equal(this.element.textContent.trim(), "NO");
+
+    this.set("keyOrIndex", 2);
+    assert.equal(this.element.textContent.trim(), "YES");
+  });
+
+  test("it works with objects", async function (assert) {
+    this.set("objOrArray", { foo: 1, bar: 2, baz: 3 });
+    this.set("keyOrIndex", "foo");
+
+    await render(hbs`{{if (is-last objOrArray keyOrIndex) "YES" "NO"}}`);
+    assert.equal(this.element.textContent.trim(), "NO");
+
+    this.set("keyOrIndex", "bar");
+    assert.equal(this.element.textContent.trim(), "NO");
+
+    this.set("keyOrIndex", "baz");
+    assert.equal(this.element.textContent.trim(), "YES");
   });
 });
