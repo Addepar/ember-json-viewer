1e2a8a9c319ad54e7f1b44afdcbc39c646f5bb72
Jim Schofield <oldcoyote@gmail.com>
2021-06-03 17:43:31 -0500

    Fixing deprecation warnings when setting value on computed property.

--- addon/components/entry-viewer.js
+++ addon/components/entry-viewer.js
@@ -7,13 +7,20 @@ import { isPrimitive } from "../utils/value-types";
 export default Component.extend({
   tagName: "",
   layout,
-  isExpanded: computed("depth", "collapseDepth", function () {
-    let depth = this.get("depth");
-    let collapseDepth = this.get("collapseDepth");
-    if (Number.isInteger(collapseDepth)) {
-      return depth < collapseDepth;
-    } else {
-      return true;
+  isExpanded: computed("depth", "collapseDepth",  {
+    get() {
+      let depth = this.get("depth");
+      let collapseDepth = this.get("collapseDepth");
+      if (Number.isInteger(collapseDepth)) {
+        this._isExpanded = depth < collapseDepth;
+        return this._isExpanded;
+      } else {
+        this._isExpanded = true;
+        return this._isExpanded;
+      }
+    },
+    set(key, value) {
+      return this._isExpanded = value;
     }
   }),
 
