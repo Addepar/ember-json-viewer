95c32d0899245b01e6175bbf03d4ed3f16210953
Bert De Block <bert.deblock@bagaar.be>
2021-09-29 13:57:08 +0200

    Resolve `no-curly-component-invocation` violations

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: "recommended",
   rules: {
     "no-action": "off",
-    "no-curly-component-invocation": "off",
     "no-invalid-interactive": "off",
     "no-triple-curlies": "off",
     "require-input-label": "off",
