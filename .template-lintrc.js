c054f46ce48ece17863e39aa82d77172fcd92eb5
Bert De Block <bert.deblock@bagaar.be>
2021-09-29 11:11:57 +0200

    Temporarily turn off violated Ember Template Lint rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   extends: "recommended",
+  rules: {
+    "no-action": "off",
+    "no-curly-component-invocation": "off",
+    "no-implicit-this": "off",
+    "no-invalid-interactive": "off",
+    "no-triple-curlies": "off",
+    "require-input-label": "off",
+    "simple-unless": "off",
+  },
 };
