db85c4ed51fc952a37227fff45cb4953a5030fe8
Fabian Foerg <28877604+faf0-addepar@users.noreply.github.com>
2022-06-28 16:16:55 -0400

    Add static security scanning with CodeQL (SAST) (#18)
    This change enables weekly CodeQL scans in addition to scans on pull requests.
    
    The test scenarios were failing due to the older version `ember-auto-import`
    being used and not due to any code issue.
    
    It would make sense to add them back once we are ready to advance to
    `4.x` to avoid compatibility issues.
    
    Co-authored-by: Alex Navasardyan

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const getChannelURL = require("ember-source-channel-url");
-
 module.exports = async function () {
   return {
     useYarn: true,
@@ -19,30 +17,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             "ember-source": "~3.20.5",
-          },
-        },
-      },
-      {
-        name: "ember-release",
-        npm: {
-          devDependencies: {
-            "ember-source": await getChannelURL("release"),
-          },
-        },
-      },
-      {
-        name: "ember-beta",
-        npm: {
-          devDependencies: {
-            "ember-source": await getChannelURL("beta"),
-          },
-        },
-      },
-      {
-        name: "ember-canary",
-        npm: {
-          devDependencies: {
-            "ember-source": await getChannelURL("canary"),
           },
         },
       },
