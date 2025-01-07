057dcddbcd3e527c96389f67957e2da336a6df43
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-07 21:19:11 +0100

    CI: Add support for ember 4.x LTS versions (#50)
    * CI: Run ember 4.4, 4.8, 4.12
    
    * Fix ember-3.28 lts by downgrading some deps, drop jquery scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,19 +9,32 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-qunit': '^5.0.0',
+            '@ember/test-helpers': '^2.4.0',
           },
         },
       },
       {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            '@ember/jquery': '^1.1.0',
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
--- tests/dummy/app/deprecation-workflow.js
+++ tests/dummy/app/deprecation-workflow.js
@@ -1,5 +1,11 @@
 import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 
 setupDeprecationWorkflow({
-  workflow: [{ handler: 'throw', matchId: 'this-property-fallback' }],
+  workflow: [
+    { handler: 'throw', matchId: 'this-property-fallback' },
+    { handler: 'throw', matchId: 'remove-owner-inject' },
+    { handler: 'throw', matchId: 'deprecate-auto-location' },
+    { handler: 'throw', matchId: 'ember-string.add-package' },
+    { handler: 'throw', matchId: 'ember-string.from-ember-module' },
+  ],
 });
