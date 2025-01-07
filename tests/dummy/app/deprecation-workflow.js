027c593c2abf07db5355c07a0eba9224b155333a
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-07 11:44:41 +0100

    Fix deprecation: this-property-fallback using https://github.com/ember-codemods/ember-no-implicit-this-codemod

--- tests/dummy/app/deprecation-workflow.js
+++ tests/dummy/app/deprecation-workflow.js
@@ -1,5 +1,5 @@
 import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 
 setupDeprecationWorkflow({
-  workflow: [{ handler: 'silence', matchId: 'this-property-fallback' }],
+  workflow: [{ handler: 'throw', matchId: 'this-property-fallback' }],
 });
