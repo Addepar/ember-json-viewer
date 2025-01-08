a453982e26c546d6dc7ec7113572945200256364
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-08 09:52:48 +0100

    use ember-qunit 9 everywhere

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,7 +12,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
-            'ember-qunit': '^5.0.0',
             '@ember/test-helpers': '^2.4.0',
             '@glimmer/component': '^1.1.2',
           },
@@ -23,7 +22,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
-            'ember-qunit': '^7.0.0',
             '@glimmer/component': '^1.1.2',
           },
         },
@@ -33,7 +31,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
-            'ember-qunit': '^7.0.0',
             '@glimmer/component': '^1.1.2',
           },
         },
