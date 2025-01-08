dd1f851bc760c03feea362d09d8d865acd30cdba
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-08 09:53:23 +0100

    use glimmer@2 everywhere

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,7 +13,6 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': '~3.28.0',
             '@ember/test-helpers': '^2.4.0',
-            '@glimmer/component': '^1.1.2',
           },
         },
       },
@@ -22,7 +21,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
-            '@glimmer/component': '^1.1.2',
           },
         },
       },
@@ -31,7 +29,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
-            '@glimmer/component': '^1.1.2',
           },
         },
       },
