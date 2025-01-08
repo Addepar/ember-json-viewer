dd3f466126b8d0699f2fd4a812f6d2d9ce141bb5
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-08 09:40:06 +0100

    add note about skipBabel:qunit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,6 +16,9 @@ module.exports = function (defaults) {
 
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app, {
+    // Without the following, the build fails in CI (ie when env.CI=true) with somewhat mysterious webpack errors
+    // about failing to find exports from QUnit (and other) modules.
+    // See: https://github.com/embroider-build/embroider/issues/677/#issuecomment-813118275
     skipBabel: [
       {
         package: 'qunit',
