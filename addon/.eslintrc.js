3c611b6f3a0ead6504127622ea10ab035b72e15e
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-10 15:56:03 +0100

    Embroider: Port to v2 format: Part 1: separate test-app (#61)
    * Embroider: Port to v2 format: separate test-app
    
    This gets to step 20 of https://github.com/embroider-build/embroider/blob/main/docs/porting-addons-to-v2.md#part-1-separate-addon-from-dummy-app
    
    * Fix linting, CI
    
    This gets to step 23 of https://github.com/embroider-build/embroider/blob/main/docs/porting-addons-to-v2.md#part-1-separate-addon-from-dummy-app
    
    * volta pin yarn

---
+++ addon/.eslintrc.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
+    },
+  },
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.stylelintrc.js',
+        './.template-lintrc.js',
+        './index.js',
+        './config/**/*.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ['plugin:n/recommended'],
+    },
+  ],
+};
--- test-app/app/app.js
+++ test-app/app/app.js
@@ -2,7 +2,7 @@ import './deprecation-workflow';
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'dummy/config/environment';
+import config from './config/environment';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
